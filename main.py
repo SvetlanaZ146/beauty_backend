52f29076c5cc4eba5c0f812a25dfe0b9fb9672e9
Zaporozhets-Svitlana <zaporozhets.sveta@gmail.com>
2026-02-23 00:00:16 +0200

    Added SQLite database and GET endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
-
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 class ProductCreate(BaseModel):
     name: str
@@ -9,10 +9,60 @@ class ProductCreate(BaseModel):
     ingredients: str
 
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return{"message": "Hellow, FastAPI"}
+
+DATABASE_URL = "sqlite:///./beauty.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker( bind = engine )
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(Text)
+    ingredient = Column(Text)
+
+Base.metadata.create_all(bind = engine)
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str
+    ingredient: str
+
+
 
 @app.post("/add_product")
 def add_product(product: ProductCreate):
-    return {"status": "product added", "product": product}
+    db = SessionLocal()
+
+    existing_product = db.query(Product).filter(Product.name == product.name).first()
+    if existing_product :
+        db.close()
+        return {"error": "Product already exist"}
+    new_product = Product(
+        name = product.name,
+        description = product.description,
+        ingredient = product.ingredient
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.close()
+    return {"status": "Product save to database"}
+
+@app.get("/product/{name}")
+def get_product(name: str):
+    db = SessionLocal()
+    product = db.query(Product),filter(Product.name == name). first()
+    db.close()
+    if not product:
+        return {"error": "Product not found"}
+
+    return {
+        "name": product.name,
+        "description": product.description,
+        "ingredients": product.ingredients
+    }
