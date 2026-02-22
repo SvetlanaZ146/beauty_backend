62bb569c467e0706ada3bf9ac89de3371fc4590b
Zaporozhets-Svitlana <zaporozhets.sveta@gmail.com>
2026-02-22 21:30:59 +0200

    Added POST endpoint

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ProductCreate(BaseModel):
+    name: str
+    description: str
+    ingredients: str
 
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return{"mesage": "Hellow, FastAPI"}
+    return{"message": "Hellow, FastAPI"}
+
+@app.post("/add_product")
+def add_product(product: ProductCreate):
+    return {"status": "product added", "product": product}
